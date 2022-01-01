f89c28d4a2a6930177d015cac828b69103d42598
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-01-01 23:41:43 +0000

    try it yourself task 3.9 complete

---
+++ introducing_lists/dinner_guests.py
@@ -0,0 +1,6 @@
+guests = ["Warren Buffet", "Dan Pena", "Marquett Davon Burton"]
+
+for guest in guests:
+    print(f"Hello {guest}, you're invited to dinner\n")
+guest_len = len(guests)
+print(f"I'm inviting {guest_len} guests to dinner\n")
