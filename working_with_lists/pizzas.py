92e63c5b380aae04d5e5e2212dbf754070a8c266
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-01-03 00:30:13 +0000

    try it yourself task 4.1 complete

---
+++ working_with_lists/pizzas.py
@@ -0,0 +1,14 @@
+my_pizzas = ["BBQ Chicken", "Margherita", "Vegan margherita"]
+
+print("\n")
+
+for pizza in my_pizzas:
+    print(f"I like {pizza} pizza.")
+    message = "I really like eating pizza"
+    message2 = "I really enjoy Domino's vegan margherita"
+    message3 = "I really love pizza!"
+print("\n")
+print(message)
+print(message2)
+print(message3)
+print("\n")
