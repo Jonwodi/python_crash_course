dbbb50e51b56d9a00837f46acedf0eaec2676a6c
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-01-04 09:37:50 +0000

    try it yourself task 4.2 complete

---
+++ working_with_lists/animals.py
@@ -0,0 +1,13 @@
+animals = ["Tiger", "Lion", "cheetah"]
+
+newline = "\n"
+
+print(newline)
+for animal in animals:
+    message = f"A {animal}, is a wild animal"
+    print(message)
+
+    message2 = "These cats are all wild animals"
+print(newline)
+print(message2)
+print(newline)
