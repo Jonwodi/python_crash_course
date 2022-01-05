53d872ca24b8c43dcc8411c0e4ab3e9df4643d5e
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-01-05 09:43:57 +0000

    try it yourself task 4.10 complete

---
+++ working_with_lists/slices.py
@@ -0,0 +1,11 @@
+cubes = [number ** 3 for number in range(1, 11)]
+print("\n")
+print(cubes)
+print("\n")
+
+message1 = f"The first three items in the list are: {cubes[0:3]}"
+message2 = f"Three items from the middle of the list are: {cubes[4:7]}"
+message3 = f"The last three items in the list are: {cubes[7:10]}\n"
+print(message1)
+print(message2)
+print(message3)
