1768d9bc54325decb57c0b75ede16dd2b3ea72ec
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-01-05 09:08:52 +0000

    try it yourself task 4.7

--- working_with_lists/odd_numbers.py
+++ working_with_lists/odd_numbers.py
@@ -1,4 +1,10 @@
 number_range = range(1, 21, 2)
 
+odd_numbers = []
+
 for number in number_range:
     print(number)
+    odd_numbers.append(number)
+print("\n")
+print(odd_numbers)
+print("\n")
---
+++ working_with_lists/threes.py
@@ -0,0 +1,11 @@
+multiples_of_3 = range(3, 31, 3)
+
+multiples_of_3_list = []
+
+
+for number in multiples_of_3:
+    print(number)
+    multiples_of_3_list.append(number)
+print("\n")
+print(multiples_of_3_list)
+print("\n")
