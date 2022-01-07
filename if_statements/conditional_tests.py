9d313c3f81434939b0a4a5f480c1b89367beaa25
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-01-07 22:33:24 +0000

    try it yourself task 5.1 complete

---
+++ if_statements/conditional_tests.py
@@ -0,0 +1,32 @@
+number = 23
+print("Is number == 23 ? predict True.")
+print(number == 23)
+
+print("\nIs number == 30 ? predict False.")
+print(number == 30)
+
+print("\nIs number > 20 ? predict True.")
+print(number > 20)
+
+print("\nIs number > 30 ? predict False.")
+print(number > 30)
+
+car = "bmw"
+print("\nIs number == 'bmw' ? predict True.")
+print(car == "bmw")
+
+print("\nIs number == 'audi' ? predict False.")
+print(car == "audi")
+
+print("\nIs 'b' in 'bmw' ? predict True.")
+print("b" in "bmw")
+
+print("\nIs 'a' in 'bmw' ? predict False.")
+print("a" in "bmw")
+
+x = y = "axis"
+print("\nIs variable x & y the same ? predict True.")
+print(x == y)
+
+print("\nIs variable x > y ? predict False.")
+print(x > y)
