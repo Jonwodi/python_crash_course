91eae54588a11e1b76cd3a5fcb37a91029d055e6
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-01-08 23:19:33 +0000

    try it yourself task 5.2 complete

---
+++ if_statements/more_conditional_tests.py
@@ -0,0 +1,39 @@
+car = "BMW"
+
+print("\nIs 'B' in 'BMW' ? predict True")
+print("b" in car)
+
+print("\nIs 'zy' in 'BMW' ? predict False")
+print("zy" in car)
+
+
+car2 = "AUDI"
+lowercase = car2.lower()
+print("\nIs the variable car == 'AUDI' ? predict False")
+print("A" in lowercase)
+
+print("\nIs 5 > 3 ? predict True")
+print(5 > 3)
+
+print("\nIs 5 < 3 ? predict False")
+print(5 < 3)
+
+print("\nIs 5 >= 5 ? predict True")
+print(5 >= 5)
+
+print("\nIs 5 <= 3 ? predict False")
+print(5 <= 3)
+
+print("\nIs True & False == False ? predict this is correct")
+print(True and False)
+
+print("\nIs True or False == True ? predict this is correct")
+print(True or False)
+
+items = ["bag", "pen", "notebook"]
+
+print("\nIs 'pen' in the list called items ? predict True")
+print("pen" in items)
+
+print("\nIs 'calculator' in the list called items ? predict False")
+print("calculator" in items)
