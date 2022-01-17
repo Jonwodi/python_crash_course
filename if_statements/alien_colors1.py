87334f2eda7e38182661f37c7bb79a18cf76c1f4
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-01-17 05:26:23 +0000

    try it yourself task 5.3 update

--- if_statements/alien_colors1.py
+++ if_statements/alien_colors1.py
@@ -1,4 +1,12 @@
 alien_color = "green"
+name = "Player1"
 
 if alien_color == "green":
-    print("that p[ayer earned 5 points")
+    print(f"{name}, just earned 5 points.")
+
+if alien_color == "green":
+    pass
+
+alien_color = "red"
+if alien_color == "green":
+    print(f"{name}, just earned 5 points.")
