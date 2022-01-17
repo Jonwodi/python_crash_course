6816d5b923be4f563b05203254d2250bdc2e16f1
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-01-17 06:18:10 +0000

    try it yourself task 5.7 complete

---
+++ if_statements/favorite_fruit.py
@@ -0,0 +1,12 @@
+favorite_fruits = ["apples", "oranges", "grapes"]
+
+if "apples" in favorite_fruits:
+    print(f"You really like {favorite_fruits[0]}!")
+if "oranges" in favorite_fruits:
+    print(f"You really like {favorite_fruits[1]}!")
+if "mangoes" in favorite_fruits:
+    print("You really like mangoes!")
+if "watermelons" in favorite_fruits:
+    print("You really like watermelons!")
+if "grapes" in favorite_fruits:
+    print(f"You really like {favorite_fruits[2]}!")
