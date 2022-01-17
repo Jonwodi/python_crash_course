9ce515d5ea7a3032631ab491deac8714bff84015
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-01-17 05:32:10 +0000

    try it yourself task 5.4 complete

---
+++ if_statements/alien_colors2.py
@@ -0,0 +1,14 @@
+alien_color = "green"
+name = "Player1"
+
+if alien_color == "green":
+    print(f"{name}, just earned 5 points for shooting the alien.")
+else:
+    print(f"{name}, just earned 10 points.")
+
+alien_color = "red"
+
+if alien_color == "green":
+    print(f"{name}, just earned 5 points for shooting the alien.")
+else:
+    print(f"{name}, just earned 10 points.")
