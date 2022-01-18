bf9dd50e490354365115c21e10e94e5f9bc60c85
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-01-18 23:14:55 +0000

    try it yourself task 5.8 complete

---
+++ if_statements/hello_admin.py
@@ -0,0 +1,8 @@
+usernames = ["admin", "Jo97", "So96", "Jg96", "Go98"]
+
+
+for user in usernames:
+    if user == "admin":
+        print(f"Hello {user}, would you like to see a status report?")
+    else:
+        print(f"Hello {user}, thank you for logging in again.")
