3ca3d682fed9b4a130b11f12488c9f0c0a3f10d4
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-01-19 15:49:42 +0000

    try it yourself task 5.10 update

--- if_statements/checking_usernames.py
+++ if_statements/checking_usernames.py
@@ -3,7 +3,7 @@ new_users = current_users.copy()
 new_users[0:4] = "Jay97", "James96", "Steph90", "JG96"
 
 for user in new_users:
-    if user in current_users or isinstance(user, str) and user.upper() == "JG96":
+    if user in current_users or isinstance(user, str) and user.upper() == new_users[3]:
         print(f"You will need to enter a new username. {user} is no longer avaliable.")
     else:
         print(f"This username {user} is avaliable.")
