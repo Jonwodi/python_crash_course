f9f707151b2d0dfb1827c54e90da410c5efdf904
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-01-19 15:59:06 +0000

    try it yourself task 5.10 final update

--- if_statements/checking_usernames.py
+++ if_statements/checking_usernames.py
@@ -7,3 +7,13 @@ for user in new_users:
         print(f"You will need to enter a new username. {user} is no longer avaliable.")
     else:
         print(f"This username {user} is avaliable.")
+print("\n")
+
+
+current_users_lower = [user.lower() for user in current_users]
+
+for new_user in new_users:
+    if new_user.lower() in current_users_lower:
+        print(f"Sorry {new_user}, that name is taken.")
+    else:
+        print(f"Great, {new_user} is still available.")
