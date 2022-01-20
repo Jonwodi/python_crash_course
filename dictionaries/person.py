c51f578bfe334b3af619d47ce65e1fba70c886a3
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-01-20 08:59:25 +0000

    try it yourself task 6.1 complete

---
+++ dictionaries/person.py
@@ -0,0 +1,7 @@
+person = {"first_name": "Mike", "last_name": "Tyson", "age": 43, "city": "London"}
+
+print(person)
+print(person["first_name"])
+print(person["last_name"])
+print(person["age"])
+print(person["city"])
