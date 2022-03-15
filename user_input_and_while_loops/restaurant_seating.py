7c49453a3e81594b9e34b4a2c7598687a0482d95
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-03-15 07:20:49 +0000

    try it yourself task 7.2 complete

---
+++ user_input_and_while_loops/restaurant_seating.py
@@ -0,0 +1,16 @@
+# Run in the command line terminal to input user data
+staff_1 = "james"
+staff_2 = "john"
+
+guests_in_dinner_group = input(
+    f"{staff_1.title()}, how many people are in the dinner group ?:\n "
+)
+answer = int(guests_in_dinner_group)
+
+if answer > 8:
+    print(
+        f"{staff_1.title()}, there is more than 8 people. So they have to wait for a table."
+    )
+else:
+    print(f"{staff_2.title()}, there is {answer} people")
+    print(f"Ok {staff_1.title()}, we have a table of {answer} avaliable for them.")
